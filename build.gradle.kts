e31caaf8e7586833ff18e1ae3821809b72615017
jmp <jasonpenilla2@me.com>
2021-04-14 05:04:37 -0700

    Update to 21w14a

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,20 +19,21 @@ repositories {
   maven("https://maven.fabricmc.net/")
   maven("https://oss.sonatype.org/content/repositories/snapshots/")
   maven("https://repo.incendo.org/content/repositories/snapshots")
+  maven("https://repo.jpenilla.xyz/snapshots/")
 }
 
-val minecraftVersion = "1.16.5"
+val minecraftVersion = "21w14a"
 
 dependencies {
   minecraft("com.mojang", "minecraft", minecraftVersion)
   mappings(minecraft.officialMojangMappings())
   modImplementation("net.fabricmc", "fabric-loader", "0.11.1")
-  //modImplementation("net.fabricmc.fabric-api", "fabric-api", "0.32.5+1.16")
+  //modImplementation("net.fabricmc.fabric-api", "fabric-api", "0.32.7+1.17")
 
   modImplementation(include("cloud.commandframework", "cloud-fabric", "1.5.0-SNAPSHOT"))
   implementation(include("cloud.commandframework", "cloud-minecraft-extras", "1.5.0-SNAPSHOT"))
 
-  modImplementation(include("net.kyori", "adventure-platform-fabric", "4.0.0-SNAPSHOT") {
+  modImplementation(include("net.kyori", "adventure-platform-fabric", "4.0.0-21w14a-SNAPSHOT") {
     exclude("ca.stellardrift", "colonel")
   })
 
