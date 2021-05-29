c58d9a1f785a2f2ebbe24a73c8a8c3d1bbf0eadb
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-05-29 05:39:29 -0700

    release: Version 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("fabric-loom") version "0.8-SNAPSHOT"
 }
 
-version = "1.0.2-SNAPSHOT"
+version = "1.0.2"
 group = "xyz.jpenilla"
 description = "Adds commands to list, search, and get information about installed mods."
 val githubUrl = "https://github.com/jpenilla/ModsCommand"
